fc9f4e0586894bb0cef53947ed9900c1d64f3246
Ward Vandewege <ward@jhvc.com>
2020-05-31 23:01:56 -0400

    Automatically add the massmesh package repos for the correct architecture.

--- communities/massmesh/settings.sh
+++ communities/massmesh/settings.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Add the massmesh package repos. The meta script will replace the {{ pkg_arch }} string with the package architecture.
+export REPOS=`echo -en "${REPOS}\n# Massmesh package repo\nsrc/gz custom http://downloads.massmesh.net/snapshots/packages/{{ pkg_arch }}/generic"`;
+
 export PACKAGES="${PACKAGES} luci luci-theme-material" # Luci for web configuration
 export PACKAGES="${PACKAGES} haveged" # Ensure entropy
 export PACKAGES="${PACKAGES} kmod-ath10k-ct ath10k-firmware-qca988x-ct -kmod-ath10k-ct-smallbuffers" # QCA wireless firmware for mesh mode
