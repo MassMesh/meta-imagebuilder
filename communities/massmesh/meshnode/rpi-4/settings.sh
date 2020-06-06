8b6849bfacd282905da79d6dd25ea4dcf76b1064
JamesVorder <james@bip44.email>
2020-06-06 14:47:30 -0400

    remove redundant deps.

--- communities/massmesh/meshnode/rpi-4/settings.sh
+++ communities/massmesh/meshnode/rpi-4/settings.sh
@@ -5,4 +5,4 @@ export VERSION="snapshots"
 export TARGET="bcm27xx/bcm2711"
 export PROFILE="rpi-4"
 
-export PACKAGES="${PACKAGES} kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8152 python3-mm-cli autoygg-go python3-argh python3-netaddr"
+export PACKAGES="${PACKAGES} kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8152 python3-mm-cli autoygg-go"
