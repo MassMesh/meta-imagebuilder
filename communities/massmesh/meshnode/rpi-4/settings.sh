62ef4675fdac4388fbe73b2285db4c136a036681
Ward Vandewege <ward@jhvc.com>
2020-06-06 14:08:05 -0400

    Reflect updated paths upstream (for real now). Also rename build targets so we can see what is going on more easily.

--- communities/massmesh/meshnode/rpi-4/settings.sh
+++ communities/massmesh/meshnode/rpi-4/settings.sh
@@ -2,7 +2,7 @@
 
 export VERSION="snapshots"
 
-export TARGET="brcm27xx/bcm2711"
+export TARGET="bcm27xx/bcm2711"
 export PROFILE="rpi-4"
 
 export PACKAGES="${PACKAGES} kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8152 python3-mm-cli autoygg-go"
