d7aff90883eb81c20a6b438e1f269a89917e9999
Ward Vandewege <ward@jhvc.com>
2020-06-06 17:46:23 -0400

    We now have packages published for autoygg-go and mm-cli (and dependencies) on aarch64_cortex-a53.

--- communities/massmesh/meshnode/rpi-b-plus/settings.sh
+++ communities/massmesh/meshnode/rpi-b-plus/settings.sh
@@ -5,4 +5,4 @@ export VERSION="snapshots"
 export TARGET="bcm27xx/bcm2710"
 export PROFILE="rpi-3"
 
-export PACKAGES="${PACKAGES} -kmod-ath10k-ct kmod-usb-net-asix-ax88179"
+export PACKAGES="${PACKAGES} -kmod-ath10k-ct kmod-usb-net-asix-ax88179 python3-mm-cli autoygg-go"
