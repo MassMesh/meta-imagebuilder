4ca8df42f30178f09331b0e919c24f3cdb7fba27
William Fleurant <william@netblazr.com>
2020-06-11 16:20:19 -0400

    meshradio: review and left comment

--- communities/massmesh/meshradio/settings.sh
+++ communities/massmesh/meshradio/settings.sh
@@ -2,4 +2,13 @@
 
 export EXTRA_IMAGE_NAME="massmesh-meshradio"
 
-export PACKAGES="${PACKAGES} -wpad-basic batctl-full kmod-batman-adv luci-app-cjdns cjdns wpad-mesh-openssl"
+## wireless controller code: We want to deselect micro wpad package and support full feature
+export PACKAGES="${PACKAGES} -wpad-basic"
+
+## batman-adv fully blown and in-kernel layer 2 switch from ozzy hell
+export PACKAGES="${PACKAGES} batctl-full"
+export PACKAGES="${PACKAGES} kmod-batman-adv"
+
+## support encrypted radios and routing jointhemesh.net
+export PACKAGES="${PACKAGES} wpad-mesh-openssl"
+export PACKAGES="${PACKAGES} luci-app-cjdns cjdns"
