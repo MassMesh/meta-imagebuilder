8151efffcffced61abc441f8e379ed05606841b8
William Fleurant <william@netblazr.com>
2020-06-11 23:55:29 -0400

    rpi-4: investigate eeprom bootloader upgrade support

--- communities/massmesh/meshnode/rpi-4/settings.sh
+++ communities/massmesh/meshnode/rpi-4/settings.sh
@@ -4,3 +4,7 @@ export VERSION="snapshots"
 
 export TARGET="bcm27xx/bcm2711"
 export PROFILE="rpi-4"
+export PACKAGES="${PACKAGES} bcm27xx-eeprom bcm27xx-userland"
+# ~ selects the following:
+# libpci libkmod pciutils bcm27xx-userland blkid bcm27xx-eeprom
+
