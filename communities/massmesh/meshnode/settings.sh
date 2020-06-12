ae10d07c032261894d5decdedbdc440e180b6a00
William Fleurant <meshnet@protonmail.com>
2020-06-11 22:16:31 -0400

    sd: merge: build 4 pull requests (#80)
    * build: helpful message when packages out of date or packages on remote or local stale or..
    
    * meshnode: packages reviewed and updated
    
    rpi-b-plus: rpi4: cleanup -> moved to meshnode
    
    note:
    - apu2: settings.sh needs review and update
    - rpi3: removed deselect of ath10k-ct driver (?)
    - python3: deselected in favor of python3-light
    - mesh toolbox/mm-cli: we can now select the src package for py files
    
    * meta: hint for testing 19.07.3 stable, put snippet into lo function
    
    note:
    - output prints targets_url
    
    * meshradio: encrypted radio and routing packages fixed upstream
    
    * meshradio: review and left comment
    
    * rebuild
    
    * rebuild
    
    * meshnode: packages re-refresh
    
    - Cleanup comments
    - Support TLS/x509 for https://feeds/app.opkg
    - Python3-Lite: urllib logging (Deselecting Python3-*)
    
    * rebuild

--- communities/massmesh/meshnode/settings.sh
+++ communities/massmesh/meshnode/settings.sh
@@ -1,15 +1,42 @@
 #!/bin/sh
-
 export EXTRA_IMAGE_NAME="massmesh-meshnode"
 
-# Packages for all mesh nodes
 
+## Optware Packages support for TLS/SSL/x509
+export PACKAGES="${PACKAGES} libustream-mbedtls ca-bundle ca-certificates"
+
+## Routing
 export PACKAGES="${PACKAGES} yggdrasil cjdns" # mesh routing protocols yggdrasil and cjdns
-export PACKAGES="${PACKAGES} luci-app-yggdrasil luci-app-cjdns" # LuCI admin for yggdrasil and cjdns
-export PACKAGES="${PACKAGES} jq" # jq for scripting json config changes
-# export PACKAGES="${PACKAGES} tor tor-fw-helper tor-resolve torsocks" # Tor
-# export PACKAGES="${PACKAGES} nodogsplash" # Captive portal - currently breaks peering and gateway
+export PACKAGES="${PACKAGES} luci-app-yggdrasil" # LuCI admin for yggdrasil
+export PACKAGES="${PACKAGES} luci-app-cjdns" # LuCI admin for cjdns
+
+## MassMesh Packages repo.com/massmesh/mm-toolbox.git
+export PACKAGES="${PACKAGES} python3-argh"
+export PACKAGES="${PACKAGES} python3-netaddr"
+export PACKAGES="${PACKAGES} python3-mm-cli-src"
+export PACKAGES="${PACKAGES} autoygg-go"
+
+## Python3 && Deps
+export PACKAGES="${PACKAGES} python3-light python3-urllib python3-logging"
+
+## JQ fast CLI parsing one liner commands
+# ~ for OpenWrt see: jshn.sh, /lib/functions.sh
+export PACKAGES="${PACKAGES} jq"
+
+## Creates a WAN interface
+# ~ Gain IP address from USB tethered celluar phones
 export PACKAGES="${PACKAGES} kmod-usb-net-rndis" # USB Tethering
-export PACKAGES="${PACKAGES} kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8152" # usb-ethernet kernel mods
+export PACKAGES="${PACKAGES} kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8152" # USB Adapters
+
+## USB WAN Support files
+# ~ Reset utility used for tethering or jammed Wi-Fi devices
+# ~ (see LiMe opkgs: watchping / watchdog)
 export PACKAGES="${PACKAGES} usbreset" # USB HC reset (host side, soft reset)
-export PACKAGES="${PACKAGES} python3" # Python3.8
+
+
+## DESELECTED ##########################################################################
+# ~ Lost Tor support after yggdrasil CKR fixed throughput without need of proxy.
+# export PACKAGES="${PACKAGES} tor tor-fw-helper tor-resolve torsocks" # Tor
+#
+# ~ This should be re-enabled, we should be serving HTML or using opennds 9000
+# export PACKAGES="${PACKAGES} nodogsplash" # Captive portal - currently breaks peering and gateway
