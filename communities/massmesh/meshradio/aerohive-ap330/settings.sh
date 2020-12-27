685a7d061e4480c173de8cf86a065dc7937ca5b4
JamesVorder <jamesvorder@gmail.com>
2020-12-27 17:51:48 -0500

    Rename build and add eth0 to LAN

--- communities/massmesh/meshradio/aerohive-ap330/settings.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-export VERSION="snapshots"
-
-export TARGET="mpc85xx/generic"
-export PROFILE="aerohive-ap330"
---
+++ communities/massmesh/meshradio/aerohive_hiveap-330/settings.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+export VERSION="snapshots"
+
+export TARGET="mpc85xx/p1020"
+export PROFILE="aerohive_hiveap-330"
