43e7c187e9edad98a52378a362e25876ca4f9262
Ward Vandewege <ward@jhvc.com>
2021-05-08 17:49:05 -0400

    Remove unused packages from the meshradio builds, add the tcpdump package.

--- communities/massmesh/meshradio/settings.sh
+++ communities/massmesh/meshradio/settings.sh
@@ -5,10 +5,8 @@ export EXTRA_IMAGE_NAME="massmesh-meshradio"
 ## wireless controller code: We want to deselect micro wpad package and support full feature
 export PACKAGES="${PACKAGES} -wpad-basic"
 
-## batman-adv fully blown and in-kernel layer 2 switch from ozzy hell
-export PACKAGES="${PACKAGES} batctl-full"
-export PACKAGES="${PACKAGES} kmod-batman-adv"
-
 ## support encrypted radios and routing jointhemesh.net
 export PACKAGES="${PACKAGES} wpad-mesh-openssl"
-export PACKAGES="${PACKAGES} luci-app-cjdns cjdns"
+
+## add debugging tools
+export PACKAGES="${PACKAGES} tcpdump"
