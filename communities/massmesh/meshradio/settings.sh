7caaea6c1662bbadd99f89cac648d508aed7d76e
Ward Vandewege <ward@jhvc.com>
2021-05-08 19:44:27 -0400

    On the meshradio profile: add packages necessary for opkg to access https links, and add the relevant prometheus-node-exporter-lua packages.

--- communities/massmesh/meshradio/settings.sh
+++ communities/massmesh/meshradio/settings.sh
@@ -8,5 +8,11 @@ export PACKAGES="${PACKAGES} -wpad-basic"
 ## support encrypted radios and routing jointhemesh.net
 export PACKAGES="${PACKAGES} wpad-mesh-openssl"
 
+## for opkg to be able to access https links
+export PACKAGES="${PACKAGES} libustream-openssl ca-bundle ca-certificates"
+
 ## add debugging tools
 export PACKAGES="${PACKAGES} tcpdump"
+
+## add prometheus-node-exporter packages
+export PACKAGES="${PACKAGES} prometheus-node-exporter-lua prometheus-node-exporter-lua-wifi prometheus-node-exporter-lua-wifi_stations prometheus-node-exporter-lua-openwrt prometheus-node-exporter-lua-netstat"
